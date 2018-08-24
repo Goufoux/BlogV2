ec214133134bc13c59fb2d5f4534ce1b7a47f123
Goufoux <deadworldcorp@gmail.com>
2018-08-24 15:23:39 +0200

    Mise à jour des propriétés bootstrap pour le champ de recherche sur mobile et tablette

--- App/Frontend/Templates/layout.php
+++ App/Frontend/Templates/layout.php
@@ -114,7 +114,7 @@
 				</ul>
 			</div>
 			<form class="form-inline my-2 my-lg-0" type="get" action="./">
-				<input class="form-control mr-sm-2" type="search" name="bSearch" placeholder="Rechercher un Book" aria-label="Search">
+				<input class="form-control mr-sm-0 col-sm-8 col-8 col-md-8" type="search" name="bSearch" placeholder="Rechercher un Book" aria-label="Search">
 				<button class="btn btn-outline-success my-2 my-sm-0">Search</button>
 			</form>
 		</nav>
