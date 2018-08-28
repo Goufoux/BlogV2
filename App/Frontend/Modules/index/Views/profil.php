9f3f035681e030b36f7fe5d383dfc187a9e6f5be
Goufoux <deadworldcorp@gmail.com>
2018-08-28 21:00:39 +0200

    Mise à jour de l'affichage du profil

--- App/Frontend/Modules/index/Views/profil.php
+++ App/Frontend/Modules/index/Views/profil.php
@@ -4,6 +4,26 @@
 	{
 		?>
 			<article class="user">
+			<span class="error"></span>
+			<?php
+				if(!empty($_SESSION['auth']))
+				{
+					if($userView->getId() != $_SESSION['membre']->getId())
+					{
+						?>
+						<script>
+							setInterval(function() 
+							{
+								$('.possAct').load('ajax/funcUser?role=user&fData='+<?php echo $userView->getId(); ?>+'');
+							}, 1000);
+						</script>
+						<div class="possAct">
+							<button> Initialisation </button>
+						</div>
+						<?php
+					}
+				}
+			?>
 				<h3> Profil de <?php echo $userView->getPseudo(); ?> (<?php echo $style->styleCatUser($userView->getCategoryUser()); ?>)</h3>
 				<span> Inscrit le <?php echo date('d-m-Y', $userView->getDti()); ?> </span>
 				<?php
@@ -28,35 +48,76 @@
 	if($user->isAuthentificated() AND !empty($userMod))
 	{
 		?>
-		<article class="user col-lg-6 col-md-6 col-12">
-			<h3> Votre Profil: <a href="profil-<?php echo $_SESSION['membre']->getId(); ?>"> <?php echo $_SESSION['membre']->getPseudo(); ?> </a></h3>
-				<form id="formInfo">
-					<label for="mPseudo"> Pseudo </label>
-						<input type="text" class="col-lg-4" name="mPseudo" id="mPseudo" value="<?php echo $_SESSION['membre']->getPseudo(); ?>" placeholder="Pseudo" required />
-					<label for="mEmail"> E-Mail </label>
-						<input type="email" class="col-lg-4" name="mEmail" id="mEmail" value="<?php echo $_SESSION['membre']->getEmail(); ?>" placeholder="E-Mail" required />
-				</form>
-				<hr>
-				<article id="formPass">
-					<h4> Votre Pass </h4>
-					<input type="password" class="col-4" name="mOldPass" id="mOldPass" placeholder="Pass actuel" required />
-					<input type="password" class="col-4" name="mNewPass" id="mNewPass" placeholder="Nouveau Pass" required />
-					<input type="password" class="col-4" name="mConfirmPass" id="mConfirmPass" placeholder="Confirmation" required />
-					<button class="uModPass"> Changer le Pass </button>
-				</article>
-				<span class="error"> </span>
-		</article>
-		<article>
-			<h4> <?php echo $style->styleCatUser($user->getCategoryUser()); ?> <a href='?modifiedCategory'> Modifier ? </a> </h4>
-				<?php
-					if($user->getCategoryUser() >= 2)
-					{
-						?>
-							<h5> <a href="readUb" title="Consultez la liste de vos Books"> Voir vos Books ? </a> </h5>
+			<section class="row col-lg-12 col-12">
+				<article class="user col-lg-6 col-md-6 col-12">
+					<h3> Votre Profil: <a href="profil-<?php echo $_SESSION['membre']->getId(); ?>"> <?php echo $_SESSION['membre']->getPseudo(); ?> </a></h3>
+					<h3> <?php echo $style->styleCatUser($user->getCategoryUser()); ?> <a href='?modifiedCategory'> Modifier ? </a> </h3>
+						<form id="formInfo" class="row">
+								<input type="text" class="col-lg-4 col-md-4 col-sm-6 col-8" name="mPseudo" id="mPseudo" value="<?php echo $_SESSION['membre']->getPseudo(); ?>" placeholder="Pseudo" required />
+								<input type="email" class="col-lg-6 col-md-7 col-sm-6 col-8" name="mEmail" id="mEmail" value="<?php echo $_SESSION['membre']->getEmail(); ?>" placeholder="E-Mail" required />
+						</form>
+						<hr>
+						<div id="formPass" class="row">
+							<h4 class="col-10"> Votre Pass </h4>
+							<input type="password" class="col-lg-4 col-md-6 col-8" name="mOldPass" id="mOldPass" placeholder="Pass actuel" required />
+							<input type="password" class="col-lg-4 col-md-6 col-8" name="mNewPass" id="mNewPass" placeholder="Nouveau Pass" required />
+							<input type="password" class="col-lg-4 col-md-6 col-8" name="mConfirmPass" id="mConfirmPass" placeholder="Confirmation" required />
+							<button class="uModPass col-lg-4 col-md-6 col-8"> Changer le Pass </button>
+						</div>
+						<span class="error"> </span>
 						<?php
-					}
-				?>
-		</article>
+							if($user->getCategoryUser() >= 2)
+							{
+								?>
+									<h5> <a href="readUb" title="Consultez la liste de vos Books"> Voir vos Books ? </a> </h5>
+								<?php
+							}
+						?>
+				</article>
+				<article class="userFollow col-lg-6 col-md-6 col-12">
+					<h3> Vos abonnements </h3>
+						<div>
+							<h4> Utilisateurs </h4>
+							<?php 
+								if(!empty($followUser))
+								{
+									foreach($followUser as $userData)
+									{
+										?>
+											<a href="profil-<?php echo $userData['id']; ?>"> <button> <?php echo $userData['pseudo']; ?> </button> </a>	
+										<?php
+									}
+								}
+								else
+								{
+									?>
+										<h5> Rien </h5>
+									<?php
+								}
+								?>
+						</div>
+						<div>
+							<h4> Books </h4>
+								<?php
+									if(!empty($followBook))
+									{
+										foreach($followBook as $bookName)
+										{
+											?>
+												<a href="book-<?php echo $bookName['id']; ?>"> <button> <?php echo $bookName['name']; ?> </button> </a>
+											<?php
+										}
+									}
+									else
+									{
+										?>
+											<h5> Rien </h5>
+										<?php
+									}
+								?>
+						</div>
+				</article>
+			</section>
 		<?php
 	}
 ?>
