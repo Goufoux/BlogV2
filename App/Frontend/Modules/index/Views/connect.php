b724e9e855b5edc426af3e73f553876282eaad27
Goufoux <deadworldcorp@gmail.com>
2018-08-29 14:52:08 +0200

    Suppression du fichier devenu inutile

--- App/Frontend/Modules/index/Views/connect.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/frontend/connect.css" />
-<h3> Connexion / Inscription </h3>
-<form action="#" method="post" class="formConnect col-12">
-	<input type="text" id="uLogin" class="uLogin" placeholder="Pseudo" name="uLogin" required />
-	<input type="password" id="uPass" class="uPass" placeholder="Pass" name="uPass" required />
-	<input type="submit" value="Connexion / Inscription" />
-</form>
