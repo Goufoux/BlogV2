34204e335a592ccaac721c0e4a9be09afacec4be
Goufoux <deadworldcorp@gmail.com>
2018-09-11 12:13:12 +0200

    Mise à jour
    Ajout de méthode de vérif pour la création de catégorie

--- lib/Core/Form.php
+++ lib/Core/Form.php
@@ -19,6 +19,10 @@
 		const PSEUDO_LENGTH_MAX = 25;
 		const COMMENT_MIN = 5;
 		const COMMENT_MAX = 100;
+		const CATEGORY_NAME_MIN = 3;
+		const CATEGORY_NAME_MAX = 30;
+		const COMMENT_CAT_MIN = 5;
+		const COMMENT_CAT_MAX = 150;
 		
 		/*
 			verifPseudo()
@@ -150,6 +154,98 @@
 			{
 				$this->setFormError('Aucun commentaire.');
 				return false;
+			}
+		}
+		
+		/*
+			verifNameCategory()
+			vérifie la validité d'un nom d'une catégorie
+		*/
+		
+		public function verifNameCategory($nameCat)
+		{
+			if(!empty($nameCat))
+			{
+				if(is_string($nameCat))
+				{
+					if(preg_match("#^[a-zA-Z]{" . Form::CATEGORY_NAME_MIN . "," . Form::CATEGORY_NAME_MAX . "}$#", $nameCat))
+					{
+						return true;
+					}
+					else
+					{
+						if(strlen($nameCat) < Form::CATEGORY_NAME_MIN)
+						{
+							$this->setFormError('Le nom de la catégorie doit être supérieur ou égal à ' . Form::CATEGORY_NAME_MIN . ' caractères.');
+							return false;
+						}
+						else if(strlen($nameCat) > Form::CATEGORY_NAME_MAX)
+						{
+							$this->setFormError('Le nom de la catégorie doit être inférieur ou égal à ' . Form::CATEGORY_NAME_MAX . ' caractères.');
+							return false;
+						}
+						else
+						{
+							$this->setFormError('Le nom de la catégorie ne peut contenir que les lettres comprises entre A et Z. (majuscule et/ou minuscule).');
+							return false;
+						}
+					}
+				}
+				else
+				{
+					$this->setFormError('Le nom de catégorie doit être une chaîne de caractère.');
+					return false;
+				}
+			}
+			else
+			{
+				$this->setFormError('Aucun nom.');
+				return false;
+			}
+		}
+		
+		/* 
+			verifCommentCategory()
+			vérifie la validité du champ Commentaire du formulaire d'ajout de catégorie
+			
+			Retourne vrai lors de la première vérification car optionnel
+				-> changer true à false pour définir à obligatoire
+		*/
+		
+		public function verifCommentCategory($commentCat)
+		{
+			if(!empty($commentCat))
+			{
+				if(is_string($commentCat))
+				{
+					if(strlen($commentCat) >= Form::COMMENT_CAT_MIN)
+					{
+						if(strlen($commentCat) <= Form::COMMENT_CAT_MAX)
+						{
+							return true;
+						}
+						else
+						{
+							$this->setFormError('Le commentaire doit contenir ' . Form::COMMENT_CAT_MAX . ' caractères max.');
+							return false;
+						}
+					}
+					else
+					{
+						$this->setFormError('Le commentaire doit contenir au moins ' . Form::COMMENT_CAT_MIN . ' caractères.');
+						return false;
+					}
+				}
+				else
+				{
+					$this->setFormError('Le commentaire doit être une chaîne de commentaire.');
+					return false;
+				}
+			}
+			else
+			{
+				$this->setFormError('Aucun commentaire');
+				return true;
 			}
 		}
 		
